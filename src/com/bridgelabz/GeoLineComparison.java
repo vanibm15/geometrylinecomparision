ed9c900d0548f2a90a91b949e8a6551869cfa7cf
vanibm15 <vanibm15@gmail.com>
2022-04-21 23:46:38 +0530

    added:length of line

--- src/com/bridgelabz/GeoLineComparison.java
+++ src/com/bridgelabz/GeoLineComparison.java
@@ -2,7 +2,15 @@ package com.bridgelabz;
 
 public class GeoLineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to line comparison program");
+        int x1, y1 ,x2, y2;
+        double lenghtofpoints;
+        x1=1;y1=1;x2=4;y2=4;
+        lenghtofpoints=Math.sqrt((x2-x1)*(y2-y1));
+        System.out.println("length of line:"+"("+x1+","+y1+"),"+"("+x2+","+y2+")==>"+lenghtofpoints);
+        lenghtofpoints=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+        System.out.println("length of line:"+lenghtofpoints);
     }
-}
+    }
+
+
 
