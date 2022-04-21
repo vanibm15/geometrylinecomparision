de6a95d2fc3cb40d90c39991ab7d0596ff1ab20f
vanibm15 <vanibm15@gmail.com>
2022-04-21 23:42:51 +0530

    added:welcomemessage

---
+++ src/com/bridgelabz/GeoLineComparison.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class GeoLineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison program");
+    }
+}
+
